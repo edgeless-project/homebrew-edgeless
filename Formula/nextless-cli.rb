6ffd310bb10172a222c3ce0a864a5876c8abeeb2
Raphael Hetzel <hetzel@in.tum.de>
2025-07-15 15:14:55 +0200

    Fix nextless_cli syntax

--- Formula/nextless-cli.rb
+++ Formula/nextless-cli.rb
@@ -4,19 +4,20 @@ class NextlessCli < Formula
   desc "CLI tool to interact with a Nextless Controller"
   homepage "https://github.com/edgeless-project/nextless/tree/development/edgeless_cli"
   url "https://github.com/edgeless-project/nextless.git",
-    tag: "cli-0.1-wrapper2",
+    tag:      "cli-0.1-wrapper2",
     revision: "163587e16d8073df7706b21f1bdf50640782c962"
   license "MIT"
   head "https://github.com/edgeless-project/nextless.git", branch: "development"
 
   depends_on "protobuf" => :build
   depends_on "rust" => :build
-  depends_on "rustup" => :recommended
   depends_on "binaryen" => :recommended
+  depends_on "rustup" => :recommended
 
   def install
-    system "cargo", "build", "--release", "--bin", "edgeless_cli"
-    bin.install "target/release/edgeless_cli" => "nextless_cli_raw"
+    # https://users.rust-lang.org/t/is-there-a-way-to-change-the-name-of-a-binary-from-cargo-install/96512/2?u=raphaelhetzel
+    system "cargo", "install", *std_cargo_args, "--bin", "edgeless_cli", "--root", "./tmp"
+    bin.install "./tmp/edgeless_cli" => "nextless_cli_raw"
     bin.install "edgeless_cli/cli_wrapper.sh" => "nextless_cli"
   end
 end
